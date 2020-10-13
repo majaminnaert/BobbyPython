3a8b19a9ab4a56bd6d3a8f351826f61c6d8253b4
majaminnaert <93TLRs-v>
2020-10-13 16:15:02 +0200

    create function hey() in module bob

--- bob.py
+++ bob.py
@@ -1,8 +1,21 @@
+# Bob answers 'Sure.' if you ask him a question.
 #
-# Skeleton file for the Python "Bob" exercise.
+# He answers 'Whoa, chill out!' if you yell at him.
 #
+# He says 'Fine. Be that way!' if you address him without actually saying
+# anything.
+#
+# He answers 'Whatever.' to anything else.
+
+def hey(message):
+    message = message.rstrip()
+    if message.endswith("?") and not message.isupper():
+        return "Sure."
+    elif message.isupper():
+        return "Whoa, chill out!"
+    elif not message or message.isspace():
+        return "Fine. Be that way!"
+    else:
+        return "Whatever."
 
 
-def hey(what):
-
-    return
